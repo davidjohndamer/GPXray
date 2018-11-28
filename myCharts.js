13a3bcff941c4f84feb20d1ad52671db24a662cc
Andrew Loughran <2266774l@student.gla.ac.uk>
2018-11-28 22:43:10 +0000

    - Add line chart that displays loaded-in Lugano.gpx elevations

--- myCharts.js
+++ myCharts.js
@@ -1,9 +1,27 @@
+//var map = L.map('map');
+//L.tileLayer('https://www.openstreetmap.org/#map=15/55.8778/-4.2390', {
+//  attribution: 'Map data &copy; <a href="http://www.osm.org">OpenStreetMap</a>'
+//}).addTo(map);
+
+var gpx = 'Lugano.gpx'; // URL to your GPX file or the GPX itself
+
+var gpxFile = new L.GPX(gpx, {});
+
+console.log(gpxFile.get_elevation_data());
+
+var elevations = [];
+
+// extract the actual elevation numbers
+gpxFile.get_elevation_data().forEach(function(element) {
+    elevations.push(element[1]);
+  });
+
 var data = {
     // A labels array that can contain any sort of values
     label: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
     // Our series array that contains series objects or in this case series data arrays
     series: [
-      [5, 2, 4, 2, 0]
+        elevations
     ]
   };
   
